ebe2fd4250cf9fe6da46fc8c156d391bcd509adb
Riyanto <riyanto.droider@gmail.com>
2026-09-16 13:26:51 +0700

    Modifikasi filament-resource bagian PositionTable

--- app/Filament/Resources/Positions/Tables/PositionsTable.php
+++ app/Filament/Resources/Positions/Tables/PositionsTable.php
@@ -3,8 +3,10 @@
 namespace App\Filament\Resources\Positions\Tables;
 
 use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
@@ -34,7 +36,9 @@ class PositionsTable
                 //
             ])
             ->recordActions([
+                ViewAction::make(),
                 EditAction::make(),
+                DeleteAction::make()
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
